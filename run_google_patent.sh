b4e5a917bd9ac6c46647abc68e132ef48028e1eb
Thowwafi <thowwafi.a@wistkey.dev>
2024-06-12 22:47:57 +0700

    Refactor code to include print statement for publication number and index

--- run_google_patent.sh
+++ run_google_patent.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Define the name of the script
-script_name="google_patent.py"
+script_name="copy_google_patent.py"
 
 # Define the log files for NL and US
 log_file_NL_30000="/home/ubuntu/google-patent-scraper/output_NL_30000.log"
 log_file_NL_40000="/home/ubuntu/google-patent-scraper/output_NL_40000.log"
+log_file_NL_50000="/home/ubuntu/google-patent-scraper/output_NL_50000.log"
 log_file_US="/home/ubuntu/google-patent-scraper/output_US.log"
 
 # Change directory to google-patent-scraper
@@ -19,11 +20,17 @@ if pgrep -f "$script_name --country_code NL --from_index 30000 --to_index 40000"
     echo "Another instance of the script with country code NL is already running. Exiting."
 else
     # Run the Python script with nohup for NL
-    nohup python -u google_patent.py --country_code NL --from_index 30000 --to_index 40000 > "$log_file_NL_30000" 2>&1 &
+    nohup python -u copy_google_patent.py --country_code NL --from_index 30000 --to_index 40000 > "$log_file_NL_30000" 2>&1 &
 fi
 
 if pgrep -f "$script_name --country_code NL --from_index 40000 --to_index 50000" > /dev/null; then
     echo "Another instance of the script with country code NL is already running. Exiting."
 else
-    nohup python -u google_patent.py --country_code NL --from_index 40000 --to_index 50000 > "$log_file_NL_40000" 2>&1 &
+    nohup python -u copy_google_patent.py --country_code NL --from_index 40000 --to_index 50000 > "$log_file_NL_40000" 2>&1 &
+fi
+
+if pgrep -f "$script_name --country_code NL --from_index 50000 --to_index 60000" > /dev/null; then
+    echo "Another instance of the script with country code NL is already running. Exiting."
+else
+    nohup python -u copy_google_patent.py --country_code NL --from_index 50000 --to_index 60000 > "$log_file_NL_50000" 2>&1 &
 fi
