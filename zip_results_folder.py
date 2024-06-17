b7bda3e628c9a76b77979eb266fe5ae7ae64f1c2
Thowwafi <thowwafi.a@wistkey.dev>
2024-06-17 21:24:59 +0700

    chore: Refactor code to add zip functionality for a specified folder

---
+++ zip_results_folder.py
@@ -0,0 +1,28 @@
+import os
+import zipfile
+import argparse
+
+def zip_folder(folder_path, zip_path):
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Zip a folder and its contents")
+    parser.add_argument("folder_to_zip", type=str, help="The path of the folder to zip")
+    parser.add_argument("zip_file_name", type=str, help="The desired path of the output zip file")
+
+    args = parser.parse_args()
+
+    folder_to_zip = args.folder_to_zip
+    zip_file_name = args.zip_file_name
+    
+    # Check if the folder exists
+    if os.path.isdir(folder_to_zip):
+        zip_folder(folder_to_zip, zip_file_name)
+        print(f"{folder_to_zip} has been zipped successfully into {zip_file_name}")
+    else:
+        print(f"The folder '{folder_to_zip}' does not exist")
