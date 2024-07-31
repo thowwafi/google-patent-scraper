106a2ebe560c1227abe3b3c8828931ff3786412c
Thowwafi Alfiansyah <thowwafi@brandsarelive.com>
2024-07-31 12:52:09 +0700

    Refactor code to fix database file name format in convert_sql_to_csv.py

--- convert_sql_to_csv.py
+++ convert_sql_to_csv.py
@@ -11,7 +11,7 @@ def export_table_to_csv(conn, table_name, csv_file_name):
 
 def main(from_index, to_index):
     # Connect to the SQLite database
-    database_file = f'patent_data_US_{from_index, to_index}.db'
+    database_file = f'patent_data_US_{from_index}_{to_index}.db'
     # check if the database file exists
     try:
         conn = sqlite3.connect(database_file)
@@ -47,10 +47,10 @@ def main(from_index, to_index):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--from_index", help="From index to get the data", type=int, default=130
+        "--from_index", help="From index to get the data", type=int,
     )
     parser.add_argument(
-        "--to_index", help="To index to get the data", type=int, default=140
+        "--to_index", help="To index to get the data", type=int,
     )
     args = parser.parse_args()
     from_index = args.from_index
